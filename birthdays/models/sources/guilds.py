6196f7bcb5b758495986f5b43e5c3ec09ce22c6a
fako <email@fakoberkers.nl>
2015-06-20 14:17:48 +0200

    Split name for NBA.

--- birthdays/models/sources/guilds.py
+++ birthdays/models/sources/guilds.py
@@ -1,8 +1,16 @@
+import re
+
 from ..person import PersonSource
 
 
 class NBASource(PersonSource):
-    pass
+
+    def split_full_name(self):
+        match = re.match("(?P<last_name>\w+), (?P<initials>[A-Z.]+) ?(?P<prefix>[\w\s]+)?", self.full_name)
+        self.last_name = match.group("last_name")
+        self.initials = match.group("initials")
+        self.prefix = match.group("prefix")
+
 
 class BIGSource(PersonSource):
     pass
---
+++ birthdays/tests/test_sources.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from django.test import TestCase
+
+from birthdays.models import NBASource
+
+
+class TestCombineCommand(TestCase):
+
+    def test_nba_split_name(self):
+        instance = NBASource()
+        instance.full_name = "Spek, H. van der"
+        instance.split_full_name()
+        self.assertEqual(instance.initials, "H.")
+        self.assertEqual(instance.prefix, "van der")
+        self.assertEqual(instance.last_name, "Spek")
+        instance = NBASource()
+        instance.full_name = "Berkers, F.C."
+        instance.split_full_name()
+        self.assertEqual(instance.initials, "F.C.")
+        self.assertEqual(instance.last_name, "Berkers")
