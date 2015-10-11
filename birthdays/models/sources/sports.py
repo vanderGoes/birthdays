4fc3d6b84cdbc6238cb31533647ab8e6f846da0b
fako <email@fakoberkers.nl>
2015-10-11 20:54:00 +0200

    Store initials for soccer sources.

--- birthdays/models/sources/sports.py
+++ birthdays/models/sources/sports.py
@@ -5,5 +5,5 @@ class SoccerSource(PersonSource):
 
     def split_full_name(self):
         super(SoccerSource, self).split_full_name()
-        self.initials = self.first_name
+        self.initials = self.first_name if len(self.first_name) <= 20 else None
         self.first_name = None
