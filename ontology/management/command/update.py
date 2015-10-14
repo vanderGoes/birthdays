90aff399a684d11bdac6d0a99e7c856921cf3ce6
fako <email@fakoberkers.nl>
2015-10-14 21:42:38 +0200

    Correct arguments.

--- ontology/management/command/update.py
+++ ontology/management/command/update.py
@@ -40,7 +40,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        ontology_type = django_apps.get_model(app_label="ontology", model_name=options["ontology"])
-        assert issubclass(ontology_type, OntologyItem), "Specified ontology type {} is not a subclass of OntologyItem".format(options["ontology"])
-        handler = getattr(self, options["update_type"])
+        ontology_type = django_apps.get_model(app_label="ontology", model_name=options["update_type"])
+        assert issubclass(ontology_type, OntologyItem), "Specified ontology type {} is not a subclass of OntologyItem".format(options["update_type"])
+        handler = getattr(self, options["ontology"])
         handler(ontology_type)
