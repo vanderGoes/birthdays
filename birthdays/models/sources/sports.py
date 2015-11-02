4a0005ec08857bbda36719bf7484fbd264cbd871
fako <email@fakoberkers.nl>
2015-11-02 16:37:05 +0100

    Allow None for first name for soccer source.

--- birthdays/models/sources/sports.py
+++ birthdays/models/sources/sports.py
@@ -5,7 +5,7 @@ class SoccerSource(PersonSource):
 
     def split_full_name(self):
         super(SoccerSource, self).split_full_name()
-        self.initials = self.first_name if len(self.first_name) <= 20 else None
+        self.initials = self.first_name if self.first_name is not None and len(self.first_name) <= 20 else None
         self.first_name = None
 
 
