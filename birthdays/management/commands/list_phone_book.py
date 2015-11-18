6522d6a0b3c982d6d58a675643cd781a633074b3
fako <email@fakoberkers.nl>
2015-11-18 22:31:27 +0100

    Include WieOWie in list.

--- birthdays/management/commands/list_phone_book.py
+++ birthdays/management/commands/list_phone_book.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 from django.core.management.base import BaseCommand
 
-from birthdays.models import NBASource, PhoneBookSource
+from birthdays.models import NBASource, PhoneBookSource, WieOWieSource
 
 
 class Command(BaseCommand):
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     @staticmethod
     def list_phone_book():
-        for accountant in NBASource.objects.all()[:100]:
+        for accountant in NBASource.objects.all()[:1000]:
             full_name = accountant.full_name
             birth_date = accountant.birth_date
             last_name = accountant.last_name
@@ -20,14 +20,22 @@ class Command(BaseCommand):
             if not city:
                 continue
             city = city.lower().capitalize()
-            query_set = PhoneBookSource.objects.filter(last_name=last_name, props__city=city)
+            phonebook_set = PhoneBookSource.objects.filter(last_name=last_name, props__city=city)
             print(full_name, birth_date)
-            for address in query_set:
+            print(10*"*" + " addresses " + 10*"*")
+            for address in phonebook_set:
                 print(
                     address.props["streetname"],
                     address.props["housenumber"],
                     address.props["postalcode"],
                     address.props["city"]
+                )
+            print(10*"*" + " wieowie " + 10*"*")
+            wieowie_set = WieOWieSource.objects.filter(last_name=last_name, props__city=city)
+            for info in wieowie_set:
+                print(
+                    info.props["age"],  # nice opportunity here :)
+                    info.props["about_me"]
                 )
             print()
 
