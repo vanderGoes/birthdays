e8310cd23a9faf3d33fe34192a289d28b871f727
fako <email@fakoberkers.nl>
2015-11-28 13:33:38 +0100

    Add fifty plus source.

---
+++ birthdays/migrations/0013_fiftyplus.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('birthdays', '0012_knacsource_musicsocietysource_triathlonsource'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FiftyPlus',
+            fields=[
+                ('personsource_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='birthdays.PersonSource')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('birthdays.personsource',),
+        ),
+    ]
